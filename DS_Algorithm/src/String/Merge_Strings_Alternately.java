9d819b9fc5e8a74dd3139d128c00585dafe1eaf0
ANKUSH <ANKUSH@DESKTOP-NISAF97>
2022-07-10 13:59:38 +0530

    DSA

---
+++ DS_Algorithm/src/String/Merge_Strings_Alternately.java
@@ -0,0 +1,17 @@
+package String;
+
+public class Merge_Strings_Alternately {
+	public String mergeAlternately(String word1, String word2) {
+        StringBuilder sb=new StringBuilder("");
+        int n=word1.length()+word2.length();
+        for(int i=0;i<n;i++){
+            if(i<word1.length()){
+                sb.append(word1.charAt(i));
+            }
+            if(i<word2.length()){
+                sb.append(word2.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+}
